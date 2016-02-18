4691471a3a5e573ca78cef94c14a2daeb4613f37
Ian Walter <ianwalter@fastmail.com>
2016-02-18 16:53:30 -0500

    Checking if Vundle exists

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Install Vundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Install or update Vundle.
+if [ -d ~/.vim/bundle/Vundle.vim ]; then
+  cd ~/.vim/bundle/Vundle.vim
+  git pull origin master
+else
+  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 
 # Concatenate base and plugin configurations into a single vim configuration.
 cat vimrcs/plugins.vim vimrcs/base.vim > ~/.vimrc
 
 if [[ $? == 0 ]]; then
   echo "Installed vim configuration successfully."
-fi
+fi`
