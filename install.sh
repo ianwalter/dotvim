d668235458cc527bbd5104f55205c43860bdbc58
Ian Walter <ianwalter@fastmail.com>
2016-02-18 17:53:31 -0500

    fixing Vundle installation

--- install.sh
+++ install.sh
@@ -2,15 +2,14 @@
 
 # Install or update Vundle.
 if [ -d ~/.vim/bundle/Vundle.vim ]; then
-  cd ~/.vim/bundle/Vundle.vim
-  git pull origin master
+  git -C ~/.vim/bundle/Vundle.vim pull origin master
 else
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
 # Concatenate base and plugin configurations into a single vim configuration.
-cat vimrcs/plugins.vim vimrcs/base.vim > ~/.vimrc
+cat vimrcs/base.vim vimrcs/plugins.vim > ~/.vimrc
 
 if [[ $? == 0 ]]; then
   echo "Installed vim configuration successfully."
-fi`
+fi
